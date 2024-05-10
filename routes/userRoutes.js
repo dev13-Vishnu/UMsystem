b72998e71608b85a4e9feb6acf42862a3b5dcd02
Vishnu P S <vishnu13three@gamil.com>
2024-05-10 21:05:30 +0530

    user authentication done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const config = require('../config/config');
 
 user_route.use(session({secret:config.sessionSecret}))
 
+const auth = require('../middlewares/auth');
+
 user_route.set('view engine','ejs');
 user_route.set('views','./views/users');
 
@@ -14,16 +16,16 @@ user_route.use(bodyparser.urlencoded({extended:true}));
 
 
 const userController = require('../controllers/userController');
-user_route.get('/register',userController.loadRegister);
+user_route.get('/register',auth.isLogout,userController.loadRegister);
 
 user_route.post('/register',userController.insertUser);
 
-user_route.get('/',userController.loginLoad);
-user_route.get('/login',userController.loginLoad);
+user_route.get('/',auth.isLogout,userController.loginLoad);
+user_route.get('/login',auth.isLogout,userController.loginLoad);
 
 user_route.post('/login',userController.verifyLogin);
 
-user_route.get('/home',userController.loadHome);
+user_route.get('/home',auth.isLogin,userController.loadHome);
 
 
 module.exports = user_route;
